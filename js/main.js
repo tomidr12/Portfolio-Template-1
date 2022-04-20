d027f75060485b3539ad177d883500e5c463ec14
TuNombre <delriotomas12@gmail.com>
2022-04-19 23:14:11 -0300

    identado x3

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 /* ===== MENU SHOW Y HIDDEN ===== */
 const navMenu = document.getElementById('nav-menu')
-    toggleMenu = document.getElementById('nav-toggle')
-    closeMenu = document.getElementById('nav-close')
+toggleMenu = document.getElementById('nav-toggle')
+closeMenu = document.getElementById('nav-close')
 
 //SHOW
-toggleMenu.addEventListener('click', () =>{
+toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show')
 })
 
 //HIDDEN
-closeMenu.addEventListener('click', () =>{
+closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show')
 })
 
@@ -17,7 +17,7 @@ closeMenu.addEventListener('click', () =>{
 /* ===== REMOVE MENU ===== */
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -27,18 +27,18 @@ const sections = document.querySelectorAll('section[id]')
 
 window.addEventListener('scroll', scrollActive)
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50
         sectionId = current.getAttribute('id')
-        
-        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
-            document.querySelector('nav__menu a[href*='+ sectionId +']').classList.add('active')
-        }else{
-            document.querySelector('nav__menu a[href*='+ sectionId +']').classList.remove('active')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        } else {
+            document.querySelector('nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
     })
 
